85d9a8cf2cbf0dcb788f0d11ce8299bc8b1cd0f9
antimattercorrade <kush991013@gmail.com>
2019-10-25 23:40:46 +0530

    seventh

---
+++ project_1/project_1.srcs/sources_1/new/radius.v
@@ -0,0 +1,71 @@
+//`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////////
+//// Company: 
+//// Engineer: 
+//// 
+//// Create Date: 10/25/2019 05:06:59 PM
+//// Design Name: 
+//// Module Name: radius
+//// Project Name: 
+//// Target Devices: 
+//// Tool Versions: 
+//// Description: 
+//// 
+//// Dependencies: 
+//// 
+//// Revision:
+//// Revision 0.01 - File Created
+//// Additional Comments:
+//// 
+////////////////////////////////////////////////////////////////////////////////////
+
+
+//module radius(input clock,input [16:0] addr,output rd
+
+//    );
+//    reg flag=1;
+//    reg [7:0] r;
+//    reg read = 0;
+//    wire [16:0] tempaddr=addr;
+//    //reg [16:0] addra=addr;
+//	wire [7:0] in1 = 0;
+//	wire [7:0] out2;
+//	blk_mem_gen_0  inst1(
+//    .clka(clock), 
+//    .wea(read), 
+//    .addra(addr), 
+//    .ena(1),
+//    .dina(in1), 
+//    .douta(out2) 
+//     );
+//     wire [7:0] val= (addr==tempaddr)?{out2[7], out2[6], out2[5], out2[4], out2[3], out2[2], out2[1], out2[0]}:8'd0;
+     
+//    //output reg [15:0] temp=0;
+//    reg [15:0] temp1=1;
+    
+//    always @(posedge clock)
+//        begin	
+           
+//              if(flag==1)
+//              begin
+//                r= {out2[7], out2[6], out2[5], out2[4], out2[3], out2[2], out2[1], out2[0]};
+//                if(r!=val)
+//                    flag=0;
+                
+//                    //addra = addra + 431*temp1;
+//                    temp1<=temp1+1;
+                  
+//               end
+//             else
+//               //temp<=temp1;       
+            
+            
+   
+            
+            
+            
+            
+            
+            
+//        end
+//endmodule
