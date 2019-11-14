b60785aa70214340e7dffa864993fdebd97eabfe
antimattercorrade <kush991013@gmail.com>
2019-11-14 20:55:22 +0530

    ok

--- Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.runs/impl_1/Color_Detection.tcl
+++ Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.runs/impl_1/Color_Detection.tcl
@@ -61,102 +61,13 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param chipscope.maxJobs 2
-  create_project -in_memory -part xc7a35tcpg236-1
-  set_property board_part digilentinc.com:basys3:part0:1.1 [current_project]
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir {C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.cache/wt} [current_project]
-  set_property parent.project_path {C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.xpr} [current_project]
-  set_property ip_output_repo {{C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.cache/ip}} [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  set_property XPM_LIBRARIES XPM_MEMORY [current_project]
-  add_files -quiet {{C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.runs/synth_1/Color_Detection.dcp}}
-  read_ip -quiet {{C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.srcs/sources_1/ip/blk_mem_gen_0_1/blk_mem_gen_0.xci}}
-  read_xdc {{C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.srcs/constrs_1/new/constr1.xdc}}
-  link_design -top Color_Detection -part xc7a35tcpg236-1
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force Color_Detection_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file Color_Detection_drc_opted.rpt -pb Color_Detection_drc_opted.pb -rpx Color_Detection_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force Color_Detection_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file Color_Detection_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file Color_Detection_utilization_placed.rpt -pb Color_Detection_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file Color_Detection_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force Color_Detection_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file Color_Detection_drc_routed.rpt -pb Color_Detection_drc_routed.pb -rpx Color_Detection_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file Color_Detection_methodology_drc_routed.rpt -pb Color_Detection_methodology_drc_routed.pb -rpx Color_Detection_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file Color_Detection_power_routed.rpt -pb Color_Detection_power_summary_routed.pb -rpx Color_Detection_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file Color_Detection_route_status.rpt -pb Color_Detection_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file Color_Detection_timing_summary_routed.rpt -pb Color_Detection_timing_summary_routed.pb -rpx Color_Detection_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file Color_Detection_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file Color_Detection_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file Color_Detection_bus_skew_routed.rpt -pb Color_Detection_bus_skew_routed.pb -rpx Color_Detection_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force Color_Detection_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-  unset ACTIVE_STEP 
-}
-
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param chipscope.maxJobs 2
+  open_checkpoint Color_Detection_routed.dcp
+  set_property webtalk.parent_dir {C:/Users/Kushagra Sharma/Downloads/Digital Systems/Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.cache/wt} [current_project]
   set_property XPM_LIBRARIES XPM_MEMORY [current_project]
   catch { write_mem_info -force Color_Detection.mmi }
   write_bitstream -force Color_Detection.bit 
--- Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.runs/impl_1/runme.sh
+++ Image_Detection_ON_FPGA/Image_Detection_ON_FPGA.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log Color_Detection.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source Color_Detection.tcl -notrace
 
 
